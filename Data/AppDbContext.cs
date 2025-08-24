d50590dd201c1c204cb0dd1bf448bc48fa85940b
Ashish kumar <ashishkd9039@gmail.com>
2025-08-24 23:19:23 +0530

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Dating_App.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dating_App.Data
+{
+    /// <summary>
+    /// Represents the application's database context, providing access to the database and enabling CRUD operations for
+    /// the application's entities.
+    /// </summary>
+    /// <remarks>This class is derived from <see cref="Microsoft.EntityFrameworkCore.DbContext"/> and is
+    /// configured with the specified <see cref="Microsoft.EntityFrameworkCore.DbContextOptions"/>. It serves as the
+    /// primary entry point for interacting with the database using Entity Framework Core.</remarks>
+    /// <param name="options"></param>
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        /// <summary>
+        /// Represents the collection of <see cref="AppUser"/> entities in the database.
+        /// </summary>
+        /// <remarks>This property provides access to the <see cref="DbSet{TEntity}"/> for performing CRUD
+        /// operations on <see cref="AppUser"/> entities. Use this property to query, add, update, or delete users in
+        /// the database.</remarks>
+        public virtual DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ Entities/AppUser.cs
@@ -0,0 +1,23 @@
+﻿namespace Dating_App.Entities
+{
+    public class AppUser
+    {
+        // Using Guid for simplicity; in a real app, consider using a more robust ID generation strategy
+        /// <summary>
+        /// Gets or sets the unique identifier for the entity.
+        /// </summary>
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        // Using 'required' to ensure these properties are set during object initialization
+        /// <summary>
+        /// Gets or sets the display name of the object.
+        /// </summary>
+        public required string DisplayName { get; set; }
+
+        // Using 'required' to ensure these properties are set during object initialization
+        /// <summary>
+        /// Gets or sets the email address associated with the object.
+        /// </summary>
+        public required string Email { get; set; }
+    }
+}
